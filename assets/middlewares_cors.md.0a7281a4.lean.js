af765e2790b1cff0307aa301f22f390936dc2269
go-aurora-engine <go-aurora-engine@users.noreply.github.com>
2022-12-04 10:49:13 +0000

    deploy: 4582460efce34f57fdc3882a14c8d5fda12968d0

--- assets/middlewares_cors.md.0a7281a4.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,c as a,o as n,a as o}from"./app.2b2410c9.js";const C=JSON.parse('{"title":"\u8DE8\u57DF\u4E2D\u95F4\u4EF6","description":"","frontmatter":{},"headers":[],"relativePath":"middlewares/cors.md"}'),l={name:"middlewares/cors.md"},p=o("",3),e=[p];function r(c,t,D,F,y,A){return n(),a("div",null,e)}var d=s(l,[["render",r]]);export{C as __pageData,d as default};
