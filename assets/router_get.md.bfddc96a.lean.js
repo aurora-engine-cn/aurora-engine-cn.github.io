acedc4772c4586afa011bc53e4e8eec37ab782f8
aurora-engine-cn <aurora-engine-cn@users.noreply.github.com>
2023-04-06 10:12:04 +0000

    deploy: 5210ebb001898eacd05eedb55e9622c495cccd84

---
+++ assets/router_get.md.bfddc96a.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o as n,N as l}from"./chunks/framework.a7bfe727.js";const u=JSON.parse('{"title":"Get 请求","description":"","frontmatter":{},"headers":[],"relativePath":"router/get.md"}'),p={name:"router/get.md"},e=l("",11),o=[e];function t(r,c,i,D,F,y){return n(),a("div",null,o)}const A=s(p,[["render",t]]);export{u as __pageData,A as default};
---
+++ assets/router_group.md.76d4fa5f.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as a,o,N as n}from"./chunks/framework.a7bfe727.js";const d=JSON.parse('{"title":"路由分组","description":"","frontmatter":{},"headers":[],"relativePath":"router/group.md"}'),l={name:"router/group.md"},e=n("",3),p=[e];function r(t,c,D,A,y,C){return o(),a("div",null,p)}const i=s(l,[["render",r]]);export{d as __pageData,i as default};
