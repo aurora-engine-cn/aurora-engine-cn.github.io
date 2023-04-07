21756de454340a979ed41512d390a540dc63c9fb
aurora-engine-cn <aurora-engine-cn@users.noreply.github.com>
2023-04-07 01:52:54 +0000

    deploy: ee21b06ca324c192de7613d18140cc68514aac27

---
+++ assets/component_dep.md.daedfa67.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,c as n,o as a,N as l}from"./chunks/framework.0799945b.js";const C=JSON.parse('{"title":"依赖管理","description":"","frontmatter":{},"headers":[],"relativePath":"component/dep.md"}'),p={name:"component/dep.md"},o=l("",22),e=[o];function r(c,t,D,F,y,A){return a(),n("div",null,e)}const b=s(p,[["render",r]]);export{C as __pageData,b as default};
---
+++ assets/router_response.md.8cd3b8a9.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o,N as t}from"./chunks/framework.0799945b.js";const m=JSON.parse('{"title":"数据响应","description":"","frontmatter":{},"headers":[],"relativePath":"router/response.md"}'),r={name:"router/response.md"},d=t("",12),c=[d];function n(s,h,l,i,p,_){return o(),e("div",null,c)}const f=a(r,[["render",n]]);export{m as __pageData,f as default};
